b3a289debf6b0090a8156bc8f605e4fc44763f2e
dom <dschoerk@gmx.at>
2018-01-07 11:35:09 +0100

    switched to discord.js

--- app.js
+++ app.js
@@ -1,34 +1,30 @@
-const Discord = require('discord.io');
+const Discord = require('discord.js');
 const request = require('request');
 const config = require('./config');
 
 
 console.log(config);
 
-var bot = new Discord.Client( {
-    autorun : true,
-    token : config.bot_token
-});
-
-bot.connect();
+var bot = new Discord.Client();
+bot.login(config.bot_token);
 
 bot.on('ready', () => {
         console.log('Logged in as %s - %s\n', bot.username, bot.id);
 });
 
-bot.on("disconnected", () => {
+bot.on("disconnect", () => {
         console.log("Bot disconnected");
-        bot.connect(); //Auto reconnect
+        bot.login(config.bot_token); //Auto reconnect
 });
 
-bot.on('message', (user, userID, channelID, message, rawEvent) => {
+bot.on('message', chatmsg => {
 
-	if(message === '!panda' || message === 't!panda') {
+	if(chatmsg.content === '!panda' || chatmsg.content === 't!panda') {
 		const url = `https://api.giphy.com/v1/gifs/random?api_key=${config.giphy_token}&tag=red%20panda`;
 		request(url, (error, response, body) => {
 
 			const msg = JSON.parse(body);
-			bot.sendMessage({to: channelID, message: msg.data.image_url}, (error, response) => {
+			chatmsg.channel.send(msg.data.image_url, (error, response) => {
 	       			console.log("bot error: " + error);
 			        console.log("bot response: " + response);
 		        });
